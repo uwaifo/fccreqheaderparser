375a9e9a90e3a7411070bc569acd97644a55e945
Glitch (fccreqheaderparser) <none>
2019-08-05 12:13:45 +0000

    🏙🐯 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,46 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var parser = require('ua-parser-js')
+const expressip = require('express-ip');
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+app.use(expressip().getIpInfoMiddleware);
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API ok'});
+});
+app.get("/api/whoami",(req,res) => {
+  let reqIp = req.headers['x-forwarded-for'].split(',')[0];
+  let reqLang = req.header('accept-language');
+  let reqSoftware = parser(req.get('user-agent'));
+  res.status(200).json({
+    ipaddress: reqIp,
+    language: reqLang,
+    software: reqSoftware.ua
+  });
+})
+
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
